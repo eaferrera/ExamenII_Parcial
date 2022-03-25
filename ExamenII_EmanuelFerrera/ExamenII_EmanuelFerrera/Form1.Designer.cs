db8a26fac53f516b973d47779b6e32b89be4f694
emanuelferrera <87384731+eaferrera@users.noreply.github.com>
2022-03-25 11:52:55 -0600

    1er Cambio

---
+++ ExamenII_EmanuelFerrera/ExamenII_EmanuelFerrera/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ExamenII_EmanuelFerrera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClaveTextBox = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsuarioTextBox.Location = new System.Drawing.Point(192, 117);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(206, 22);
+            this.UsuarioTextBox.TabIndex = 0;
+            this.UsuarioTextBox.Text = "eferrera";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(124, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario:";
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.BackColor = System.Drawing.Color.Silver;
+            this.AceptarButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.AceptarButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AceptarButton.Location = new System.Drawing.Point(242, 184);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(74, 28);
+            this.AceptarButton.TabIndex = 2;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.UseVisualStyleBackColor = false;
+            this.AceptarButton.Click += new System.EventHandler(this.AceptarButton_Click);
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.CancelarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelarButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.CancelarButton.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelarButton.Location = new System.Drawing.Point(324, 184);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(74, 28);
+            this.CancelarButton.TabIndex = 5;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = false;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Clave:";
+            // 
+            // ClaveTextBox
+            // 
+            this.ClaveTextBox.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClaveTextBox.Location = new System.Drawing.Point(192, 151);
+            this.ClaveTextBox.Name = "ClaveTextBox";
+            this.ClaveTextBox.PasswordChar = '*';
+            this.ClaveTextBox.Size = new System.Drawing.Size(206, 22);
+            this.ClaveTextBox.TabIndex = 3;
+            this.ClaveTextBox.Text = "123";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.AceptarButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ExamenII_EmanuelFerrera.Properties.Resources.Login;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.CancelarButton;
+            this.ClientSize = new System.Drawing.Size(582, 299);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ClaveTextBox);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ClaveTextBox;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ExamenII_EmanuelFerrera/ExamenII_EmanuelFerrera/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Datos.Accesos;
+using Datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenII_EmanuelFerrera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // NOMBRE EL USUARIO: eferrera
+        // CLAVE: 123
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            UsuarioDA usuarioDA = new UsuarioDA();
+            Usuario usuario = new Usuario();
+
+            usuario = usuarioDA.Login(UsuarioTextBox.Text, ClaveTextBox.Text);
+
+            if (usuario == null)
+            {
+                MessageBox.Show("El usuario no existe.");
+            }
+
+            if (UsuarioTextBox.Text == "")
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese el nombre del usuario.");
+                UsuarioTextBox.Focus();
+                return;
+            }
+
+            if (ClaveTextBox.Text == "")
+            {
+                errorProvider1.SetError(ClaveTextBox, "Ingrese la clave del usuario.");
+                ClaveTextBox.Focus();
+                return;
+            }
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
